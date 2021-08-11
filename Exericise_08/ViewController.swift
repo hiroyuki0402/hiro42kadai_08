d09d7d17495624b4dd41be9d090de6baa5f682df
HiroyukiShiraishi <hiro19900402@gmail.com>
2021-08-11 16:44:17 +0900

    Initial Commit

---
+++ Exericise_08/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exericise_08
+//
+//  Created by 白石裕幸 on 2021/08/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
